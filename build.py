df09ee6e9cd1ea0cb4f174a5e232e525e5e028af
kajomano <marton.kajo@gmail.com>
2022-08-04 14:54:19 +0200

    Trying to simplify the build toolchain

--- build.py
+++ build.py
@@ -3,12 +3,19 @@ import subprocess as sp
 
 os.makedirs('./build', exist_ok = True)
 
-sp.run([
+cmd_config = [
     'cmake',
     '-S', './src',
     '-B', './build'
-])
+]
+# If on windows
+if (os.name == 'nt'): 
+    cmd_config += ['-DCMAKE_TOOLCHAIN_FILE=C:/Users/mkajo/Home/Raytracing/vcpkg/scripts/buildsystems/vcpkg.cmake']
 
+# Configure
+sp.run(cmd_config)
+
+# Build
 sp.run([
     'cmake',
     '--build', './build'
