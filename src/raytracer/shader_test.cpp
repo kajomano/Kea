cbe621721ccd6495fa4033ce703709289e44d418
kajomano <marton.kajo@gmail.com>
2024-07-04 11:36:55 +0200

    SAVE before I mess up everything with vulkan-hpp conversion

--- src/raytracer/shader_test.cpp
+++ src/raytracer/shader_test.cpp
@@ -5,46 +5,47 @@
 #include <cstdlib>
 #include <optional>
 
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan.hpp>
 
 #include "shader_test.h"
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
+// https://bakedbits.dev/posts/vulkan-compute-example/
+// https://github.com/KhronosGroup/Vulkan-Hpp
+// https://gist.github.com/PolarNick239/d4e8cf6645ccb84804afe8d864be3f22
 
-const std::vector<const char*> validationLayers = {
-    "VK_LAYER_KHRONOS_validation"
-};
+// const std::vector<const char*> validationLayers = {
+//     "VK_LAYER_KHRONOS_validation"
+// };
 
-#ifdef DISABLE_VALIDATION_LAYERS
-	const bool enableValidationLayers = false;
-#else
-	const bool enableValidationLayers = true;
-#endif
+// #ifdef DISABLE_VALIDATION_LAYERS
+// 	const bool enableValidationLayers = false;
+// #else
+// 	const bool enableValidationLayers = true;
+// #endif
 
-VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
-    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
-    if (func != nullptr) {
-        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
-    } else {
-        return VK_ERROR_EXTENSION_NOT_PRESENT;
-    }
-}
+// VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+//     auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+//     if (func != nullptr) {
+//         return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+//     } else {
+//         return VK_ERROR_EXTENSION_NOT_PRESENT;
+//     }
+// }
 
-void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
-    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
-    if (func != nullptr) {
-        func(instance, debugMessenger, pAllocator);
-    }
-}
+// void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+//     auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+//     if (func != nullptr) {
+//         func(instance, debugMessenger, pAllocator);
+//     }
+// }
 
-struct QueueFamilyIndices {
-    std::optional<uint32_t> graphicsFamily;
+// struct QueueFamilyIndices {
+//     std::optional<uint32_t> graphicsFamily;
 
-    bool isComplete() {
-        return graphicsFamily.has_value();
-    }
-};
+//     bool isComplete() {
+//         return graphicsFamily.has_value();
+//     }
+// };
 
 class HelloTriangleApplication {
 public:
@@ -56,16 +57,16 @@ public:
 
 private:
     VkInstance instance;
-	VkDebugUtilsMessengerEXT debugMessenger;
-	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-	VkDevice device;
-    VkQueue graphicsQueue;
+	// VkDebugUtilsMessengerEXT debugMessenger;
+	// VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	// VkDevice device;
+    // VkQueue graphicsQueue;
 
     void initVulkan() {
         createInstance();
-		setupDebugMessenger();
-		pickPhysicalDevice();
-		createLogicalDevice();
+		// setupDebugMessenger();
+		// pickPhysicalDevice();
+		// createLogicalDevice();
     }
 
     void draw() {
@@ -73,19 +74,19 @@ private:
     }
 
     void cleanup() {
-		vkDestroyDevice(device, nullptr);
+		// vkDestroyDevice(device, nullptr);
 
-		if (enableValidationLayers) {
-            DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
-        }
+		// if (enableValidationLayers) {
+        //     DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+        // }
 
         vkDestroyInstance(instance, nullptr);
     }
 
     void createInstance() {
-        if (enableValidationLayers && !checkValidationLayerSupport()) {
-            throw std::runtime_error("Validation layers requested, but not available!");
-        }
+        // if (enableValidationLayers && !checkValidationLayerSupport()) {
+        //     throw std::runtime_error("Validation layers requested, but not available!");
+        // }
 
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -99,176 +100,176 @@ private:
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
 
-        auto extensions = getRequiredExtensions();
-        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-        createInfo.ppEnabledExtensionNames = extensions.data();
+        // auto extensions = getRequiredExtensions();
+        // createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        // createInfo.ppEnabledExtensionNames = extensions.data();
 
-        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
-        if (enableValidationLayers) {
-            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-            createInfo.ppEnabledLayerNames = validationLayers.data();
+        // VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+        // if (enableValidationLayers) {
+        //     createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        //     createInfo.ppEnabledLayerNames = validationLayers.data();
 
-            populateDebugMessengerCreateInfo(debugCreateInfo);
-            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
-        } else {
-            createInfo.enabledLayerCount = 0;
+        //     populateDebugMessengerCreateInfo(debugCreateInfo);
+        //     createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
+        // } else {
+        //     createInfo.enabledLayerCount = 0;
 
-            createInfo.pNext = nullptr;
-        }
+        //     createInfo.pNext = nullptr;
+        // }
 
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
             throw std::runtime_error("Failed to create instance!");
         }
     }
 
-    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
-        createInfo = {};
-        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-        createInfo.pfnUserCallback = debugCallback;
-    }
+    // void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+    //     createInfo = {};
+    //     createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    //     createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    //     createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    //     createInfo.pfnUserCallback = debugCallback;
+    // }
 
-    void setupDebugMessenger() {
-        if (!enableValidationLayers) return;
+    // void setupDebugMessenger() {
+    //     if (!enableValidationLayers) return;
 
-        VkDebugUtilsMessengerCreateInfoEXT createInfo;
-        populateDebugMessengerCreateInfo(createInfo);
+    //     VkDebugUtilsMessengerCreateInfoEXT createInfo;
+    //     populateDebugMessengerCreateInfo(createInfo);
 
-        if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
-            throw std::runtime_error("Failed to set up debug messenger!");
-        }
-    }
+    //     if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+    //         throw std::runtime_error("Failed to set up debug messenger!");
+    //     }
+    // }
 
-	void pickPhysicalDevice() {
-        uint32_t deviceCount = 0;
-        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+	// void pickPhysicalDevice() {
+    //     uint32_t deviceCount = 0;
+    //     vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
 
-        if (deviceCount == 0) {
-            throw std::runtime_error("Failed to find GPUs with Vulkan support!");
-        }
+    //     if (deviceCount == 0) {
+    //         throw std::runtime_error("Failed to find GPUs with Vulkan support!");
+    //     }
 
-        std::vector<VkPhysicalDevice> devices(deviceCount);
-        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+    //     std::vector<VkPhysicalDevice> devices(deviceCount);
+    //     vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
-        for (const auto& device : devices) {
-            if (isDeviceSuitable(device)) {
-                physicalDevice = device;
-                break;
-            }
-        }
+    //     for (const auto& device : devices) {
+    //         if (isDeviceSuitable(device)) {
+    //             physicalDevice = device;
+    //             break;
+    //         }
+    //     }
 
-        if (physicalDevice == VK_NULL_HANDLE) {
-            throw std::runtime_error("Failed to find a suitable GPU!");
-        }
-    }
+    //     if (physicalDevice == VK_NULL_HANDLE) {
+    //         throw std::runtime_error("Failed to find a suitable GPU!");
+    //     }
+    // }
 
-   	void createLogicalDevice() {
-        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+   	// void createLogicalDevice() {
+    //     QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
 
-        VkDeviceQueueCreateInfo queueCreateInfo{};
-        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
-        queueCreateInfo.queueCount = 1;
+    //     VkDeviceQueueCreateInfo queueCreateInfo{};
+    //     queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    //     queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+    //     queueCreateInfo.queueCount = 1;
 
-        float queuePriority = 1.0f;
-        queueCreateInfo.pQueuePriorities = &queuePriority;
+    //     float queuePriority = 1.0f;
+    //     queueCreateInfo.pQueuePriorities = &queuePriority;
 
-        VkPhysicalDeviceFeatures deviceFeatures{};
+    //     VkPhysicalDeviceFeatures deviceFeatures{};
 
-        VkDeviceCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    //     VkDeviceCreateInfo createInfo{};
+    //     createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
 
-        createInfo.pQueueCreateInfos = &queueCreateInfo;
-        createInfo.queueCreateInfoCount = 1;
+    //     createInfo.pQueueCreateInfos = &queueCreateInfo;
+    //     createInfo.queueCreateInfoCount = 1;
 
-        createInfo.pEnabledFeatures = &deviceFeatures;
+    //     createInfo.pEnabledFeatures = &deviceFeatures;
 
-        createInfo.enabledExtensionCount = 0;
+    //     createInfo.enabledExtensionCount = 0;
 
-        if (enableValidationLayers) {
-            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-            createInfo.ppEnabledLayerNames = validationLayers.data();
-        } else {
-            createInfo.enabledLayerCount = 0;
-        }
+    //     if (enableValidationLayers) {
+    //         createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+    //         createInfo.ppEnabledLayerNames = validationLayers.data();
+    //     } else {
+    //         createInfo.enabledLayerCount = 0;
+    //     }
 
-        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create logical device!");
-        }
+    //     if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+    //         throw std::runtime_error("failed to create logical device!");
+    //     }
 
-        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
-    }
+    //     vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+    // }
 
-    bool isDeviceSuitable(VkPhysicalDevice device) {
-        QueueFamilyIndices indices = findQueueFamilies(device);
+    // bool isDeviceSuitable(VkPhysicalDevice device) {
+    //     QueueFamilyIndices indices = findQueueFamilies(device);
 
-        return indices.isComplete();
-    }
+    //     return indices.isComplete();
+    // }
 
-    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
-        QueueFamilyIndices indices;
+    // QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+    //     QueueFamilyIndices indices;
 
-        uint32_t queueFamilyCount = 0;
-        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+    //     uint32_t queueFamilyCount = 0;
+    //     vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
 
-        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
-        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+    //     std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    //     vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
 
-        int i = 0;
-        for (const auto& queueFamily : queueFamilies) {
-            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-                indices.graphicsFamily = i;
-            }
+    //     int i = 0;
+    //     for (const auto& queueFamily : queueFamilies) {
+    //         if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+    //             indices.graphicsFamily = i;
+    //         }
 
-            if (indices.isComplete()) {
-                break;
-            }
+    //         if (indices.isComplete()) {
+    //             break;
+    //         }
 
-            i++;
-        }
+    //         i++;
+    //     }
 
-        return indices;
-    }
+    //     return indices;
+    // }
 
-    std::vector<const char*> getRequiredExtensions() {
-        std::vector<const char*> extensions;
+    // std::vector<const char*> getRequiredExtensions() {
+    //     std::vector<const char*> extensions;
 
-        if (enableValidationLayers) {
-            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-        }
+    //     if (enableValidationLayers) {
+    //         extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    //     }
 
-        return extensions;
-    }
+    //     return extensions;
+    // }
 
-    bool checkValidationLayerSupport() {
-        uint32_t layerCount;
-        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    // bool checkValidationLayerSupport() {
+    //     uint32_t layerCount;
+    //     vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
-        std::vector<VkLayerProperties> availableLayers(layerCount);
-        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+    //     std::vector<VkLayerProperties> availableLayers(layerCount);
+    //     vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
-        for (const char* layerName : validationLayers) {
-            bool layerFound = false;
+    //     for (const char* layerName : validationLayers) {
+    //         bool layerFound = false;
 
-            for (const auto& layerProperties : availableLayers) {
-                if (strcmp(layerName, layerProperties.layerName) == 0) {
-                    layerFound = true;
-                    break;
-                }
-            }
+    //         for (const auto& layerProperties : availableLayers) {
+    //             if (strcmp(layerName, layerProperties.layerName) == 0) {
+    //                 layerFound = true;
+    //                 break;
+    //             }
+    //         }
 
-            if (!layerFound) return false;
-        }
+    //         if (!layerFound) return false;
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
-    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
-        std::cerr << "Vulkan validation layer: " << pCallbackData->pMessage << std::endl;
+    // static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+    //     std::cerr << "Vulkan validation layer: " << pCallbackData->pMessage << std::endl;
 
-        return VK_FALSE;
-    }
+    //     return VK_FALSE;
+    // }
 };
 
 int shaderTest() {
